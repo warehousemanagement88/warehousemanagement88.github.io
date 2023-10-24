d9e21e34751e7df620d5766b691a2be9a6891e38
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 17:32:01 +0700

    push post.js

--- togcf/post.js
+++ togcf/post.js
@@ -2,4 +2,3 @@
 // import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import PostSignUp from "./postfunction.js";
 window.PostSignUp = PostSignUp;
-document.getElementById("button").addEventListener("click", PostSignUp);
