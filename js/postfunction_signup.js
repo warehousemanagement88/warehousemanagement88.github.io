2cc82b32c45de87eb6df3abc48969de17eb7a1ed
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 20:07:25 +0700

    test

--- js/postfunction_signup.js
+++ js/postfunction_signup.js
@@ -34,3 +34,73 @@ export default function PostSignUp() {
       // Handle errors here, if needed
     });
 }
+
+import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+export default function postSignUp() {
+  firstname = getValue("firstname");
+  lastname = getValue("lastname");
+  email = getValue("email");
+  password = getValue("password");
+  confirmpass = getValue("confirmpass");
+
+
+  // Validasi isian tidak boleh kosong
+  if (!firstname || !lastname || !email || !password || !confirmpass) {
+    Swal.fire({
+      icon: "error",
+      title: "Signup Failed",
+      text: "Please fill in all fields.",
+    });
+    return;
+  }
+
+  let target_url = "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehousesignup";
+  let datainjson = {
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    password: password,
+    confirmpass: confirmpass,
+  };
+
+  fetch(target_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datainjson),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      responseData(result);
+    })
+    .catch((error) => {
+      // Handle errors (e.g., network issues)
+      console.error("Error:", error);
+    })
+    .finally(() => {
+    });
+}
+
+function responseData(result) {
+  if (result) {
+    // Tampilkan SweetAlert berhasil signUp
+    Swal.fire({
+      icon: "success",
+      title: "SignUp Successful",
+      text: "You have successfully signed up.",
+    }).then((result) => {
+      if (result.isConfirmed || result.isDismissed) {
+        window.location.href = "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
+      }
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Signup Failed",
+      text: result.message,
+    });
+  }
+}
