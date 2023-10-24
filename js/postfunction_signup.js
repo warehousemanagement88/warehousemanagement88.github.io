5532ec1edf80a287b4187ae5f2ee7a0601f88517
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 20:11:14 +0700

    bismillah

--- js/postfunction_signup.js
+++ js/postfunction_signup.js
@@ -1,40 +1,3 @@
-// import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-// import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
-
-export default function PostSignUp() {
-  let target_url =
-    "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehousesignup";
-  let datainjson = {
-    firstname: getValue("firstname"),
-    lastname: getValue("lastname"),
-    email: getValue("email"),
-    password: getValue("password"),
-    confirmpass: getValue("confirmpass"),
-  };
-
-  fetch(target_url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(datainjson),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result.message);
-      console.log(result.token);
-      console.log(result.status);
-      // setCookieWithExpireHour("token", result.token, 2);
-      window.location.href =
-        "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle errors here, if needed
-    });
-}
-
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 // import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
@@ -44,7 +7,6 @@ export default function postSignUp() {
   email = getValue("email");
   password = getValue("password");
   confirmpass = getValue("confirmpass");
-
 
   // Validasi isian tidak boleh kosong
   if (!firstname || !lastname || !email || !password || !confirmpass) {
@@ -56,7 +18,8 @@ export default function postSignUp() {
     return;
   }
 
-  let target_url = "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehousesignup";
+  let target_url =
+    "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehousesignup";
   let datainjson = {
     firstname: firstname,
     lastname: lastname,
@@ -80,8 +43,7 @@ export default function postSignUp() {
       // Handle errors (e.g., network issues)
       console.error("Error:", error);
     })
-    .finally(() => {
-    });
+    .finally(() => {});
 }
 
 function responseData(result) {
@@ -93,7 +55,8 @@ function responseData(result) {
       text: "You have successfully signed up.",
     }).then((result) => {
       if (result.isConfirmed || result.isDismissed) {
-        window.location.href = "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
+        window.location.href =
+          "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
       }
     });
   } else {
