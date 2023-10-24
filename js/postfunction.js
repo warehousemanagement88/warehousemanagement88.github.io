bf4ee70d7b4715e409bb2b1273e25f81961d6e8f
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 21:29:36 +0700

    bismillah

--- js/postfunction.js
+++ js/postfunction.js
@@ -1,7 +1,6 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
-import Swal from "sweetalert2";
 
 export default function PostSignIn() {
   let target_url =
@@ -14,25 +13,27 @@ export default function PostSignIn() {
     password: getValue("password"),
   };
 
-  // Validasi isian tidak boleh kosong
-  if (!email || !password) {
-    Swal.fire({
-      icon: "error",
-      title: "Login Failed",
-      text: "Please fill in all fields.",
-    });
-    return;
-  }
-
   postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
 }
 
 function responseData(result) {
-  //setInner("pesan", result.message);
-  console.log(result.message);
-  console.log(result.token);
-  console.log(result.status);
-  setCookieWithExpireHour("token", result.token, 2);
-  window.location.href =
-    "https://warehousemanagement88.github.io/warehousesystem/template/dashboard.html";
+  if (result.status === true) {
+    Swal.fire({
+      icon: "success",
+      title: "SignIn Successful",
+      text: "You have successfully signed in.",
+    }).then((result) => {
+      if (result.isConfirmed || result.isDismissed) {
+        window.location.href =
+          "https://warehousemanagement88.github.io/warehousesystem/template/dashboard.html";
+      }
+    });
+  } else {
+    // Tampilkan SweetAlert ketika signup gagal
+    Swal.fire({
+      icon: "error",
+      title: "SignIn Failed",
+      text: result.message || "An error occurred during sign in.",
+    });
+  }
 }
