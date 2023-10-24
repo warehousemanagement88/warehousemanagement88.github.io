b766446859de056e825b2b13d92f3972cf43fae2
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 19:28:49 +0700

    project3

--- js/postfunction_signup.js
+++ js/postfunction_signup.js
@@ -1,16 +1,19 @@
-import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+// import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
-export default function PostSignIn() {
+export default function PostSignUp() {
   let target_url =
-    "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehouselogin";
-  let tokenkey = "token";
-  let tokenvalue =
-    "5c633cfc6243cec8c9b5dae4a8aae7b366126ad04ee4e5a90c7596e7f8b9a9d8";
+    "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehousesignup";
+  // let tokenkey = "token";
+  // let tokenvalue =
+  //   "5c633cfc6243cec8c9b5dae4a8aae7b366126ad04ee4e5a90c7596e7f8b9a9d8";
   let datainjson = {
+    firstname: getValue("firstname"),
+    lastname: getValue("lastname"),
     email: getValue("email"),
     password: getValue("password"),
+    confirmpass: getValue("confirmpass"),
   };
 
   postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
@@ -23,5 +26,5 @@ function responseData(result) {
   console.log(result.status);
   setCookieWithExpireHour("token", result.token, 2);
   window.location.href =
-    "https://warehousemanagement88.github.io/warehousesystem/template/dashboard.html";
+    "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
 }
