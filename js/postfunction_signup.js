6b09e94f6b166ebaf33e5b1ffb856fcbe7b58cdc
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 19:49:18 +0700

    test

--- js/postfunction_signup.js
+++ js/postfunction_signup.js
@@ -1,13 +1,10 @@
-import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+// import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
+// import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp() {
   let target_url =
     "https://asia-southeast2-nyoba-402410.cloudfunctions.net/warehousesignup";
-  // let tokenkey = "token";
-  // let tokenvalue =
-  //   "5c633cfc6243cec8c9b5dae4a8aae7b366126ad04ee4e5a90c7596e7f8b9a9d8";
   let datainjson = {
     firstname: getValue("firstname"),
     lastname: getValue("lastname"),
@@ -16,15 +13,24 @@ export default function PostSignUp() {
     confirmpass: getValue("confirmpass"),
   };
 
-  postWithToken(target_url, datainjson, responseData);
-}
-
-function responseData(result) {
-  //setInner("pesan", result.message);
-  console.log(result.message);
-  console.log(result.token);
-  console.log(result.status);
-  setCookieWithExpireHour("token", result.token, 2);
-  window.location.href =
-    "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
+  fetch(target_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datainjson),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result.message);
+      console.log(result.token);
+      console.log(result.status);
+      // setCookieWithExpireHour("token", result.token, 2);
+      window.location.href =
+        "https://warehousemanagement88.github.io/warehousesystem/login/index.html";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle errors here, if needed
+    });
 }
