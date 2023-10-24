d3b6544fb0e99e1b4c1443e719efaf84727f0d10
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 16:02:49 +0700

    project3

--- api/togcf/postfunction.js
+++ api/togcf/postfunction.js
@@ -17,7 +17,7 @@ export default function PostSignUp() {
 }
 
 function responseData(result) {
-  setInner("pesan", result.message);
+  //setInner("pesan", result.message);
   setCookieWithExpireHour("token", result.token, 2);
   window.location.href = "dashboard.html";
 }
