cee8b8cd9f15ceca31662b10b9f1b4f3223a99a9
Novi <alnoviantiramadhani91@gmail.com>
2023-10-24 15:28:16 +0700

    project3

---
+++ api/togcf/post.js
@@ -0,0 +1,4 @@
+// import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+// import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+import PostSignUp from "./postfunction.js";
+window.PostSignUp = PostSignUp;
---
+++ api/togcf/postfunction.js
@@ -0,0 +1,21 @@
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
+
+export default function PostSignUp() {
+  let target_url =
+    "https://asia-southeast2-awangga.cloudfunctions.net/petapedia-post";
+  let tokenkey = "token";
+  let tokenvalue = "8e87pod9d9a8fh9sfd87f9dhsf98dsf98sdf9ssd98f";
+  let datainjson = {
+    username: getValue("username"),
+    password: getValue("password"),
+  };
+
+  postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
+}
+
+function responseData(result) {
+  setInner("pesan", result.message);
+  setCookieWithExpireHour("token", result.token, 2);
+}
