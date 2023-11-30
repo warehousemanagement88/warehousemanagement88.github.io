119e300cf4b8b42801b2efd61d03616c0ada4792
Novi <alnoviantiramadhani91@gmail.com>
2023-11-30 22:31:51 +0700

    Update JS

--- build/pages/admin/gudang_a/assets/js/getFunction/getFunctionGudangA.js
+++ build/pages/admin/gudang_a/assets/js/getFunction/getFunctionGudangA.js
@@ -8,7 +8,7 @@ export const tableGudangA = `
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
   <div class="flex px-2 py-1">
     <div>
-      <img src="../../assets/img/adidas.JPG" class="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-soft-in-out h-9 w-9 rounded-xl" alt="user1" />
+      <img src="../assets/img/adidas.JPG" class="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-soft-in-out h-9 w-9 rounded-xl" alt="user1" />
     </div>
     <div class="flex flex-col justify-center">
       <h6 class="mb-0 text-sm leading-normal">#NAME#</h6>
@@ -57,7 +57,7 @@ export function responseData(results) {
 
 export function isiRow(value) {
   const gudanga = tableGudangA
-    .replace("#NAMA#", value.name)
+    .replace("#NAME#", value.name)
     .replace("#CATEGORY#", value.category)
     .replace("#QTY#", value.qty)
     .replace("#SKU#", value.sku)
