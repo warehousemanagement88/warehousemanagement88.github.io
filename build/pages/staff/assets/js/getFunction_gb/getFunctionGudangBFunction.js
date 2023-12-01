efc0cb0d027cb2f0f8c1173f228a0af275cbefd3
Novi <alnoviantiramadhani91@gmail.com>
2023-12-02 02:34:59 +0700

    Mengupdate frontend

---
+++ build/pages/staff/assets/js/getFunction_gb/getFunctionGudangBFunction.js
@@ -0,0 +1,21 @@
+import { URLGetGudangB, responseData } from "./getFunctionGudangB.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", getCookie("Authorization"));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
+
+get(URLGetGudangB, responseData);
