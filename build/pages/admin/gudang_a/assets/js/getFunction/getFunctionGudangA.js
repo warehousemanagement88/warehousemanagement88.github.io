dbe96181fe6054b9632477f306eff2e69e8f680b
Novi <alnoviantiramadhani91@gmail.com>
2023-12-01 22:38:55 +0700

    Update delete gudang a

--- build/pages/admin/gudang_a/assets/js/getFunction/getFunctionGudangA.js
+++ build/pages/admin/gudang_a/assets/js/getFunction/getFunctionGudangA.js
@@ -46,7 +46,9 @@ export const tableGudangA = `
 </td>
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
   <a type="button" href="edit_gudang_a.html?gudangaId=#EDIT#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-edit"></i></a>
-  <button class="text-xs font-semibold leading-tight text-slate-400">#DELETE#</button>
+  <button onclick="deleteGudangA(#DELETE#)" class="text-xs font-semibold leading-tight text-slate-400">
+  <i class="fas fa-trash-alt"></i>
+</button>
 </td>
 </tr>
 `;
