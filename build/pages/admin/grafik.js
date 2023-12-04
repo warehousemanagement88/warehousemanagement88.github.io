473e66f71ef239694639ac15f8b80f2c3ee245dc
Novi <alnoviantiramadhani91@gmail.com>
2023-12-04 22:55:40 +0700

    Membuat grafik

---
+++ build/pages/admin/grafik.js
@@ -0,0 +1,50 @@
+function show_chart(target_url, idChart, backgroundColor) {
+  fetch(target_url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      const temps = result;
+      const ctx = document.getElementById(idChart);
+      const products = temps.map((temp) => temp.name);
+      const values = temps.map((temp) => temp.qty);
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: products,
+          datasets: [
+            {
+              label: "Data",
+              data: values,
+              borderWidth: 1,
+              backgroundColor: backgroundColor,
+            },
+          ],
+        },
+        options: {
+          indexAxis: "y",
+        },
+      });
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+}
+show_chart(
+  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangb",
+  "myChart-a",
+  "rgba(255, 99, 132, 0.2)"
+);
+show_chart(
+  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangb",
+  "myChart-b",
+  "rgba(54, 162, 235, 0.2)"
+);
+show_chart(
+  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangc",
+  "myChart-c",
+  "rgba(255, 206, 86, 0.2)"
+);
