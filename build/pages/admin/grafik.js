da0a73fe11bdc1b05daf1858e861c45a9dcebbb6
Novi <alnoviantiramadhani91@gmail.com>
2023-12-04 23:10:50 +0700

    Mengupdate Grafik

--- build/pages/admin/grafik.js
+++ build/pages/admin/grafik.js
@@ -1,4 +1,4 @@
-function show_chart(target_url, idChart, backgroundColor) {
+function show_chart(target_url, idChart, backgroundColor, warehouse_total) {
   fetch(target_url, {
     method: "GET",
     headers: {
@@ -7,8 +7,10 @@ function show_chart(target_url, idChart, backgroundColor) {
   })
     .then((response) => response.json())
     .then((result) => {
+      document.getElementById(warehouse_total).innerHTML = result.length;
       const temps = result;
       const ctx = document.getElementById(idChart);
+
       const products = temps.map((temp) => temp.name);
       const values = temps.map((temp) => temp.qty);
       new Chart(ctx, {
@@ -36,15 +38,18 @@ function show_chart(target_url, idChart, backgroundColor) {
 show_chart(
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangb",
   "myChart-a",
-  "rgba(255, 99, 132, 0.2)"
+  "rgba(255, 99, 132, 0.2)",
+  "total-gudanga"
 );
 show_chart(
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangb",
   "myChart-b",
-  "rgba(54, 162, 235, 0.2)"
+  "rgba(54, 162, 235, 0.2)",
+  "total-gudangb"
 );
 show_chart(
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangc",
   "myChart-c",
-  "rgba(255, 206, 86, 0.2)"
+  "rgba(255, 206, 86, 0.2)",
+  "total-gudangc"
 );
