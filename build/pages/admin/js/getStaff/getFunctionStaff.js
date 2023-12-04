29f29221524b0ac88cfe237bd12a9108c4742bdb
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 01:42:25 +0700

    update js

--- build/pages/admin/js/getStaff/getFunctionStaff.js
+++ build/pages/admin/js/getStaff/getFunctionStaff.js
@@ -1,23 +1,23 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 export const URLGetStaff =
-  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_user";
+  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_staff";
 
 export const tableStaff = `
 <tr>
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-  <p class="mb-0 text-xs font-semibold leading-tight">#FIRSTNAME#</p>
+  <p class="mb-0 text-xs font-semibold leading-tight">#NAMALENGKAP#</p>
 </td>
 <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-  <span class="text-xs font-semibold leading-tight text-slate-400">#LASTNAME#</span>
+  <span class="text-xs font-semibold leading-tight text-slate-400">#JABATAN#</span>
+</td>
+<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+  <span class="text-xs font-semibold leading-tight text-slate-400">#JENISKELAMIN#</span>
 </td>
 <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
   <span class="text-xs font-semibold leading-tight text-slate-400">#EMAIL#</span>
 </td>
 <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
   <span class="text-xs font-semibold leading-tight text-slate-400">#PASSWORD#</span>
-</td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-  <span class="text-xs font-semibold leading-tight text-slate-400">#ROLE#</span>
 </td>
 </tr>
 `;
@@ -29,10 +29,10 @@ export function responseData(results) {
 
 export function isiRow(value) {
   const staff = tableStaff
-    .replace("#FIRSTNAME#", value.firstname)
-    .replace("#LASTNAME#", value.lastname)
+    .replace("#NAMALENGKAP#", value.namalengkap)
+    .replace("#JABATAN#", value.jabatan)
+    .replace("#JENISKELAMIN#", value.jeniskelamin)
     .replace("#EMAIL#", value.email)
-    .replace("#PASSWORD#", value.password)
-    .replace("#ROLE#", value.role);
+    .replace("#PASSWORD#", value.password);
   addInner("tableStaff", staff);
 }
