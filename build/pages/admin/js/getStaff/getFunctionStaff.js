658cbc4a42b2d82c3e0c45dbc22fb7e2e6cfdc22
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 09:26:04 +0700

    Update staff

--- build/pages/admin/js/getStaff/getFunctionStaff.js
+++ build/pages/admin/js/getStaff/getFunctionStaff.js
@@ -3,8 +3,21 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 export const URLGetStaff =
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_user";
 
+// Add the new URL
+export const URLGetStaffStaff =
+  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_staff";
+
 export const tableStaff = `
 <tr>
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+  <p class="mb-0 text-xs font-semibold leading-tight">#NAMALENGKAP#</p>
+</td>
+<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+  <span class="text-xs font-semibold leading-tight text-slate-400">#JABATAN#</span>
+</td>
+<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+  <span class="text-xs font-semibold leading-tight text-slate-400">#JENISKELAMIN#</span>
+</td>
 <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
   <span class="text-xs font-semibold leading-tight text-slate-400">#EMAIL#</span>
 </td>
@@ -24,8 +37,11 @@ export function responseData(results) {
 
 export function isiRow(value) {
   const staff = tableStaff
-    .replace("#EMAIL#", value.email)
-    .replace("#PASSWORD#", value.password)
-    .replace("#ROLE#", value.role);
+    .replace("#NAMALENGKAP#", value.staff.namalengkap)
+    .replace("#JABATAN#", value.staff.jabatan)
+    .replace("#JENISKELAMIN#", value.staff.jeniskelamin)
+    .replace("#EMAIL#", value.user.email)
+    .replace("#PASSWORD#", value.user.password)
+    .replace("#ROLE#", value.user.role);
   addInner("tableStaff", staff);
 }
