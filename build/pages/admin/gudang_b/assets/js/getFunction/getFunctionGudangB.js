3ce2bb1d56dd52ce86388d8570dca2a1641eb334
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 19:42:31 +0700

    Update js

--- build/pages/admin/gudang_b/assets/js/getFunction/getFunctionGudangB.js
+++ build/pages/admin/gudang_b/assets/js/getFunction/getFunctionGudangB.js
@@ -6,7 +6,7 @@ export const URLGetGudangB =
 
 export const tableGudangB = `
 <tr>
-<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <div class="flex px-2 py-1">
     <div>
       <img src="../../../assets/img/adidas.JPG" class="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-soft-in-out h-9 w-9 rounded-xl" alt="user1" />
@@ -17,31 +17,31 @@ export const tableGudangB = `
     </div>
   </div>
 </td>
-<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <p class="mb-0 text-xs font-semibold leading-tight">#QTY#</p>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#SKU#</span>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#SELLINGPRICE#</span>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#ORIGINALPRICE#</span>
 </td>
-<td class="p-2 text-sm leading-normal text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="bg-gradient-to-tl from-green-600 to-lime-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white">#AVAILABILITY#</span>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#COLOR#</span>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#BREADCRUMBS#</span>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#BRAND#</span>
 </td>
-<td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <span class="text-xs font-semibold leading-tight text-slate-400">#DATE#</span>
 </td>
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
