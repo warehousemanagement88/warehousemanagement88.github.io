d8eb7fdacf396dab4d363f0b223b6d28ea47829a
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 11:18:11 +0700

    Update detail

---
+++ build/pages/admin/js/getStaff/detail_staff.js
@@ -0,0 +1,28 @@
+// detail_staff.js
+
+// Function to fetch staff details and populate the details section
+export function fetchAndPopulateStaffDetails(staffId) {
+  const url = `https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_staff/${staffId}`;
+
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((result) => populateDetails(result))
+    .catch((error) => {
+      console.error("Error fetching staff details:", error);
+    });
+}
+
+// Function to populate the details section with staff details
+function populateDetails(details) {
+  // Update the DOM elements within the card
+  const detailNamaLengkap = document.getElementById("detailNamaLengkap");
+  const detailJabatan = document.getElementById("detailJabatan");
+  const detailJenisKelamin = document.getElementById("detailJenisKelamin");
+
+  detailNamaLengkap.textContent = details.namalengkap || "";
+  detailJabatan.textContent = details.jabatan || "";
+  detailJenisKelamin.textContent = details.jeniskelamin || "";
+  // Update more details as needed
+}
--- build/pages/admin/js/getStaff/getFunctionStaff.js
+++ build/pages/admin/js/getStaff/getFunctionStaff.js
@@ -15,8 +15,8 @@ export const tableStaff = `
   <span class="text-xs font-semibold leading-tight text-slate-400">#ROLE#</span>
 </td>
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-<a type="button" href="detail_staff.html?staffId=#DETAIL#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-info-circle"></i> Detail</a>
-<a type="button" href="edit_staff.html?staffId=#EDIT#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-edit"></i> Edit</a>
+<a type="button" href="detail_staff.html?staffId=#DETAIL#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-info-circle"></i></a>
+<a type="button" href="edit_staff.html?staffId=#EDIT#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-edit"></i></a>
 </button>
 </tr>
 `;
