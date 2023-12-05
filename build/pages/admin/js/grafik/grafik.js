e17122a1aed580280301065867ceec85b0706a64
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 21:47:26 +0700

    Update js

--- build/pages/admin/js/grafik/grafik.js
+++ build/pages/admin/js/grafik/grafik.js
@@ -7,20 +7,20 @@ function show_chart(target_url, idChart, backgroundColor, warehouse_total) {
   })
     .then((response) => response.json())
     .then((result) => {
+      document.getElementById(warehouse_total).innerHTML = result.length;
       const temps = result;
       const ctx = document.getElementById(idChart);
 
       const products = temps.map((temp) => temp.name);
       const values = temps.map((temp) => temp.qty);
-      console.log(products); // Periksa data label
-
+      console.log(values);
       new Chart(ctx, {
         type: "bar",
         data: {
           labels: products,
           datasets: [
             {
-              label: "Stock",
+              label: "Data",
               data: values,
               borderWidth: 1,
               backgroundColor: backgroundColor,
@@ -29,14 +29,6 @@ function show_chart(target_url, idChart, backgroundColor, warehouse_total) {
         },
         options: {
           indexAxis: "y",
-          scales: {
-            xAxes: [
-              {
-                categoryPercentage: 0.6,
-                barPercentage: 0.8,
-              },
-            ],
-          },
         },
       });
     })
@@ -44,22 +36,18 @@ function show_chart(target_url, idChart, backgroundColor, warehouse_total) {
       console.log("error", error);
     });
 }
-
-// Panggil fungsi show_chart untuk setiap grafik
 show_chart(
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudanga",
   "myChart-a",
   "rgba(255, 99, 132, 0.2)",
   "total-gudanga"
 );
-
 show_chart(
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangb",
   "myChart-b",
   "rgba(54, 162, 235, 0.2)",
   "total-gudangb"
 );
-
 show_chart(
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_gudangc",
   "myChart-c",
