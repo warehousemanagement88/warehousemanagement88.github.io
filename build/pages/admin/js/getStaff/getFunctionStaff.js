c81392e0fe83c0933e482d208b129d39038c9c1d
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 09:59:08 +0700

    Update staff

--- build/pages/admin/js/getStaff/getFunctionStaff.js
+++ build/pages/admin/js/getStaff/getFunctionStaff.js
@@ -3,10 +3,6 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 export const URLGetStaff =
   "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_user";
 
-// Add the new URL
-export const URLGetStaffStaff =
-  "https://asia-southeast2-warehousemanagement88.cloudfunctions.net/warehouse_staff";
-
 export const tableStaff = `
 <tr>
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
@@ -36,17 +32,12 @@ export function responseData(results) {
 }
 
 export function isiRow(value) {
-  if (value && value.staff && value.user) {
-    const staff = tableStaff
-      .replace("#NAMALENGKAP#", value.staff.namalengkap || "")
-      .replace("#JABATAN#", value.staff.jabatan || "")
-      .replace("#JENISKELAMIN#", value.staff.jeniskelamin || "")
-      .replace("#EMAIL#", value.user.email || "")
-      .replace("#PASSWORD#", value.user.password || "")
-      .replace("#ROLE#", value.user.role || "");
-
-    addInner("tableStaff", staff);
-  } else {
-    console.error("Invalid data format or missing properties:", value);
-  }
+  const staff = tableStaff
+    .replace("#NAMALENGKAP#", value.staff.namalengkap)
+    .replace("#JABATAN#", value.staff.jabatan)
+    .replace("#JENISKELAMIN#", value.staff.jeniskelamin)
+    .replace("#EMAIL#", value.user.email)
+    .replace("#PASSWORD#", value.user.password)
+    .replace("#ROLE#", value.user.role);
+  addInner("tableStaff", staff);
 }
