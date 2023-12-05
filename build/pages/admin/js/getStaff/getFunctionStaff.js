67dcdf08ee9bd8a4f4c1fae27789f4407df5d085
Novi <alnoviantiramadhani91@gmail.com>
2023-12-05 10:17:25 +0700

    Update js

--- build/pages/admin/js/getStaff/getFunctionStaff.js
+++ build/pages/admin/js/getStaff/getFunctionStaff.js
@@ -14,6 +14,10 @@ export const tableStaff = `
 <td class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
   <span class="text-xs font-semibold leading-tight text-slate-400">#ROLE#</span>
 </td>
+<td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+<a type="button" href="detail_staff.html?staffId=#DETAIL#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-info-circle"></i> Detail</a>
+<a type="button" href="edit_staff.html?staffId=#EDIT#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-edit"></i> Edit</a>
+</button>
 </tr>
 `;
 
@@ -24,8 +28,10 @@ export function responseData(results) {
 
 export function isiRow(value) {
   const staff = tableStaff
-    .replace("#EMAIL#", value.user.email)
-    .replace("#PASSWORD#", value.user.password)
-    .replace("#ROLE#", value.user.role);
+    .replace("#EMAIL#", value.email)
+    .replace("#PASSWORD#", value.password)
+    .replace("#ROLE#", value.role)
+    .replace("#DETAIL#", value._id)
+    .replace("#EDIT#", value._id);
   addInner("tableStaff", staff);
 }
